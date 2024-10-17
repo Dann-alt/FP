8abf34e7ab58b0b84ac97171f498e53cfc7e5089
Dan Garcia <danna.garciago@my.unitec.edu.mx>
2024-10-17 12:38:57 -0600

    Create Entregable-21DeOct.cpp

---
+++ Introduccion/Entregable-21DeOct.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+	// Ejercicio 1.1 
+
+	cout << "(3 + 5 * 2 / 2 – 1): " << (3 + 5 * 2 / 2 - 1) << endl; 
+	cin >> 
+
+	// Ejercicio 1.2 
+
+	// Ejercicio 1.3
+
+	// Ejercicio 1.4
+
+	// Ejercicio 1.5
+
+	// Ejercicio 1.6
+
+	// Ejercicio 1.7
+
+
+}
