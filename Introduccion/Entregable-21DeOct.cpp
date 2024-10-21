08aefa9f6320dcde7e2466592ab7eb32666fb49d
Dan Garcia <danna.garciago@my.unitec.edu.mx>
2024-10-21 12:41:18 -0600

    Update Entregable-21DeOct.cpp

--- Introduccion/Entregable-21DeOct.cpp
+++ Introduccion/Entregable-21DeOct.cpp
@@ -6,6 +6,18 @@ int main()
 
 	// Ejercicio 1.1 
 
+	double resultado1, resultado2, resultado3, resultado4, resultado5;
+
+	resultado1 = 3 + 5 * 2 / 2 - 1;
+	cout << "Resultado de 3 + 5 * 2 / 2 - 1: " << resultado1 << endl;
+	resultado2 = 3 + 10 / 2 - 1;
+	cout << "Resultado de 3 + 10 / 2 - 1: " << resultado2 << endl;
+	resultado3 = 3 + 5 - 1;
+	cout << "Resultado de 3 + 5 - 1: " << resultado3 << endl;
+	resultado4 = 8 - 1;
+	cout << "Resultado de 8 - 1: " << resultado4 << endl;
+	resultado5 = 1.0 / 3 * 3;
+	cout << "Resultado de 1.0 / 3 * 3: " << resultado5 << endl;
 
 	// Ejercicio 1.2 
 
