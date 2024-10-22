cf19865b9e79e9132a8c16280309745d56603365
Dan Garcia <danna.garciago@my.unitec.edu.mx>
2024-10-21 22:46:52 -0600

    Update Entregable-21DeOct.cpp

--- Introduccion/Entregable-21DeOct.cpp
+++ Introduccion/Entregable-21DeOct.cpp
@@ -29,6 +29,28 @@ int main()
 
 	// Ejercicio 1.6
 
+	// 1. 3*4
+	 int ResultadoM = 3 * 4;
+	 cout << "3 * 4 = " << ResultadoM << endl;
+	// 2. 12 / 3
+	 int ResultadoD = 12 / 3;
+	 cout << "12 / 3 = " << ResultadoD << endl;
+	// 3. 12 % 3
+	 int Porcentaje1 = 12 % 3;
+	 cout << "12 % 3 = " << Porcentaje1 << endl;
+	// 4. 12 % 7
+	 int Porcentaje2 = 12 % 7;
+	 cout << "12 % 7 = " << Porcentaje2 << endl;
+	// 5. 3 + 3.0
+	 float ResultadoS = 3 + 3.0;
+	 cout << "3 + 3.0 = " << ResultadoS << endl;
+	// 6. 10 / 100
+	 float DivisionD = 10.0 / 100;
+	 cout << "10 / 100 = " << DivisionD << endl;
+	// 7. Potencia 3 a la 2 usando pow
+	double Pot = pow(3, 2);
+	cout << "3^2 = " << Pot << endl;
+
 	// Ejercicio 1.7
 
 
