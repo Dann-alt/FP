bd12676c422fc41bb34f786428fc9a82fac82288
Dan Garcia <danna.garciago@my.unitec.edu.mx>
2024-10-24 12:39:34 -0600

    Update Entregable-24DeOctubre.cpp

--- Introduccion/Entregable-24DeOctubre.cpp
+++ Introduccion/Entregable-24DeOctubre.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	// EJERCICIO 1.1
+
+	int numero;
+	cout << "Ingrese un numero: ";
+	cin >> numero; 
+	int resultado = numero * numero;
+	cout << "El resultado es: " << resultado << endl; 
+
+	// EJERCICIO 1.2
+	
+	int numero;
+	cout << "Ingrese un numero: ";
+	cin >> numero;
+	int resultado = numero * 2; 
+	cout << "El resultado es: " << resultado << endl;
+	int resultado = numero * 3;
+	cout << "El resultado es: " << resultado << endl;
+	int resultado = numero * 4;
+	cout << "El resultado es: " << resultado << endl;
+	int resultado = numero * 5;
+	cout << "El resultado es: " << resultado << endl;
+
+	// EJERCICIO 1.3
+	
+
+
+	// EJERCICIO 1.4
+	
+
+
+	// EJERCICIO 1.5
+	
+
+
+	// EJERCICIO 1.6
+	
+
+
+	// EJERCICIO 1.7
+	
+
+
+	// EJERCICIO 1.8
+	
+
+
+	// EJERCICIO 1.9
+	
+
+
+	// EJERCICIO 1.10
+
+
+}
 
