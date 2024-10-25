c529a6dc98ce9334c0015b04aab80c3919783b5d
Dan Garcia <danna.garciago@my.unitec.edu.mx>
2024-10-24 23:31:15 -0600

    Update Entregable-24DeOctubre.cpp

--- Introduccion/Entregable-24DeOctubre.cpp
+++ Introduccion/Entregable-24DeOctubre.cpp
@@ -15,25 +15,46 @@ int main()
 
 	// EJERCICIO 1.2
 	
-	int numero;
-	cout << "Ingrese un numero: ";
-	cin >> numero;
-	int resultado = numero * 2; 
-	cout << "El resultado es: " << resultado << endl;
-	int resultado = numero * 3;
-	cout << "El resultado es: " << resultado << endl;
-	int resultado = numero * 4;
-	cout << "El resultado es: " << resultado << endl;
-	int resultado = numero * 5;
-	cout << "El resultado es: " << resultado << endl;
+	int x;
+	cout << "Ingresa un número entero: ";
+	cin >> x;
+	cout << "Resultados:" << endl;
+	cout << "x = " << x << endl;
+	cout << "2x = " << 2 * x << endl;
+	cout << "3x = " << 3 * x << endl;
+	cout << "4x = " << 4 * x << endl;
+	cout << "5x = " << 5 * x << endl;
 
 	// EJERCICIO 1.3
-	
 
+	float pesoKg, pesoLb;
+	const float conversion = 2.2;
+	
+	cout << "Ingresa tu peso en kilogramos: ";
+	cin >> pesoKg;
+	
+	pesoLb = pesoKg * conversion; 
+	cout << "Tu peso en libras es: " << pesoLb << " lb" << endl;
 
 	// EJERCICIO 1.4
-	
 
+	float coso1, coso2, coso3; 
+	float Total, Promedio;
+	
+	cout << "Ingrese el primer número: ";
+	cin >> coso1;
+	
+	cout << "Ingrese el segundo número: ";
+	cin >> coso2;
+	
+	cout << "Ingrese el tercer número: ";
+	cin >> coso3; 
+	
+	Total = coso1 + coso2 + coso3; 
+	Promedio = Total / 3;
+	
+	cout << "La suma de los tres números es: " << Total << endl;
+	cout << "El promedio de los tres números es: " << Promedio << endl;
 
 	// EJERCICIO 1.5
 	
