468fd7bd288edeb0519509f4373b2f531d2a1d01
Dan Garcia <danna.garciago@my.unitec.edu.mx>
2024-11-25 12:15:59 -0600

    Update PracticaJ.cpp

--- Introduccion/PracticaJ.cpp
+++ Introduccion/PracticaJ.cpp
@@ -24,3 +24,20 @@ int main()
 		cout << elemento << endl; 
 	}
 }
+
+//EJERCICIO 2
+{
+	int numeros[10]
+	{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+	vector <int> vnum
+	{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+	int mayor = -1; 
+	for (int i = 0; i <= 9; i++)
+	{
+		if (numeros[i] > mayor)
+		{
+			mayor = numeros[i];
+		}
+	}
+	cout << "El mayor es: " << mayor << endl;
+}
